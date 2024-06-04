218d35de6b028e18921f69295a79957070da2a94
Nixon <lazaronixon@hotmail.com>
2024-06-04 16:13:52 -0300

    initial

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## [Unreleased]
+
+## [0.1.0] - 2024-06-04
+
+- Initial release
---
+++ lib/css/zero.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "zero/version"
+
+module Css
+  module Zero
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
