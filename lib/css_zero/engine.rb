f41327ffaec7ac747fa7c06240b9b09d1598157f
Nixon <lazaronixon@hotmail.com>
2024-11-14 03:16:38 -0300

    we don't need disable css generators

--- lib/css_zero/engine.rb
+++ lib/css_zero/engine.rb
@@ -1,9 +1,5 @@
 module CssZero
   class Engine < ::Rails::Engine
-    initializer "css_zero.disable_generator_stylesheets" do
-      Rails.application.config.generators.stylesheets = false
-    end
-
     config.app_generators do |g|
       g.template_engine :css_zero
     end
